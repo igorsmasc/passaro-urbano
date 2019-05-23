92805fcb2054a3b8310c7858f356dc87726cfd23
Igor Santos Mascarenhas <Igorsmascarenhas@outlook.com>
2019-05-23 13:56:05 -0300

    Recuperando ofertas do tipo restaurante

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -9,11 +9,14 @@ export class OfertasService {
     constructor(private http: HttpClient) {}
 
     public getOfertas(): Promise<Oferta[]> {
-        // Efetuar uma requisição http
         return this.http.get('http://localhost:3000/ofertas?destaque=true')
          .toPromise()
          .then((resposta: any) => resposta);
-        // Retonar uma promise Oferta[]
     }
 
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        .toPromise()
+        .then((resposta: any) => resposta);
+    }
 }
