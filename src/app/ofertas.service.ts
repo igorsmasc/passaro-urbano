f9ec1d205a839c98ea63a3d4ecefe8ac54abf70c
Igor Santos Mascarenhas <Igorsmascarenhas@outlook.com>
2019-05-29 16:23:47 -0300

    Como usar e Onde fica

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,21 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Oferta } from './shared/oferta.model';
 
+import { ENDPOINT } from './app.api';
+
 
 @Injectable()
 export class OfertasService {
 
-    constructor(private http: HttpClient) {}
+    resposta: any;
+
+    constructor(private http: HttpClient) { }
 
     public getOfertas(): Promise<Oferta[]> {
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${ENDPOINT}?destaque=true`)
          .toPromise()
          .then((resposta: any) => resposta);
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${ENDPOINT}?categoria=${categoria}`)
         .toPromise()
         .then((resposta: any) => resposta);
     }
+
+    public getOfertaById(id: number): Promise<Oferta> {
+        return this.http.get(`${ENDPOINT}?id=${id}`)
+        .toPromise()
+        .then((resposta: Oferta) => {
+            this.resposta = JSON.stringify(resposta);
+            return resposta[0];
+        });
+    }
+
 }
