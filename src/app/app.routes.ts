f9a8f573a99a0f5507e743161336b52a106fb17e
Igor Santos Mascarenhas <Igorsmascarenhas@outlook.com>
2019-06-05 12:53:43 -0300

    Ajuste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,9 @@ export const ROUTES: Routes = [
     { path: 'restaurantes', component: RestaurantesComponent },
     { path: 'diversao', component: DiversaoComponent },
     { path: 'oferta', component: OfertaComponent},
-    { path: 'oferta/:id', component: OfertaComponent}
+    { path: 'oferta/:id', component: OfertaComponent, 
+        children: [
+            { }
+        ]
+    }
 ];
