c83a7490244ca0fdcab60eee36f05489a42ba2e7
Igor Santos Mascarenhas <Igorsmascarenhas@outlook.com>
2019-06-24 15:41:37 -0300

    Input Pesquisar

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { Observable, interval, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -11,9 +10,6 @@ import { Observable, interval, Observer, Subscription } from 'rxjs';
   providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit, OnDestroy {
-
-  private tempoObservableSub: Subscription;
-  private meuObservableTesteSub: Subscription;
 
   public oferta: Oferta;
 
@@ -33,31 +29,9 @@ export class OfertaComponent implements OnInit, OnDestroy {
    );
       */
 
-     const tempo = interval(1);
-     this.tempoObservableSub = tempo.subscribe((resposta: number ) => console.log(resposta));
-
-
-      // Observable (Observável)
-
-     const meuObservableTeste = Observable.create((observer: Observer<string>) => {
-      observer.next('Primeiro evento da stream');
-      observer.next('Segundo evento da stream');
-      observer.complete();
-     });
-
-      // Observer (Observador)
-
-     this.meuObservableTesteSub = meuObservableTeste.subscribe(
-        (resultado: any) => console.log(resultado),
-        (erro: string) => console.log(erro),
-        () => console.log('Stream finalizada')
-      );
-
     }
 
     ngOnDestroy(): void {
-      this.meuObservableTesteSub.unsubscribe();
-      this.tempoObservableSub.unsubscribe();
     }
 
 }
